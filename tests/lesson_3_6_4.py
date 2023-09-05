2eaa45e190bff74a5c65e0f1bbd68d9483864970
svirt <svirtes@gmail.com>
2023-09-05 20:43:56 +0400

    tests

--- tests/lesson_3_6_4.py
+++ tests/lesson_3_6_4.py
@@ -1,18 +1,17 @@
 import pytest
 
 import login
-import time
 from selenium.webdriver.common.by import By
 
 
 class TestStepik():
     def test_login_stepik(self, browser):
         browser.get('https://stepik.org/lesson/236895/step/1')
-        browser.implicitly_wait(5)
-        browser.find_element(By.XPATH, "//header[@id='main-header']//*[contains(@class, 'navbar__auth_login')]")
-        input1 = browser.find_element(By.XPATH, '//*[@id="id_login_email"]')
+        browser.implicitly_wait(15)
+        browser.find_element(By.XPATH, "//header[@id='main-header']//*[contains(@class, 'navbar__auth_login')]").click()
+        input1 = browser.find_element(By.XPATH, "//form[@id='login_form']//*[contains(@name, 'login')]")
         input1.send_keys(login.email)
-        input2 = browser.find_element(By.XPATH, '//*[@id="id_login_password"]')
+        input2 = browser.find_element(By.XPATH, "//form[@id='login_form']//*[contains(@name, 'password')]")
         input2.send_keys(login.password)
-        login_button = browser.find_element(By.XPATH, '//*[@id="login_form"]/button')
+        login_button = browser.find_element(By.XPATH, "//form[@id='login_form']//*[contains(@type, 'submit')]")
         login_button.click()
