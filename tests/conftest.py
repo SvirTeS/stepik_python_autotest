0c74d847fc3fbaef1e6da670e2b8edd24e3c6d8c
svirt <svirtes@gmail.com>
2023-09-06 18:25:49 +0400

    fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,19 @@
-import pytest
-from selenium import webdriver
+#import pytest
+#from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as chrome_options
 
 
-@pytest.fixture
-def get_webdriver():
-    options = chrome_options()
-    options.add_argument('chrome')
-    return options
+#@pytest.fixture
+#def get_webdriver():
+   # options = chrome_options()
+   # options.add_argument('chrome')
+  #  return options
 
 
-@pytest.fixture
-def get_webdriver(get_chrome_options):
-    options = get_chrome_options
-    driver = webdriver.Chrome(options=options)
-    return driver
+#@pytest.fixture
+#def get_webdriver(get_chrome_options):
+    #options = get_chrome_options
+   # driver = webdriver.Chrome(options=options)
+   # return driver
 
 
--- tests/lesson_3_6_5.py
+++ tests/lesson_3_6_5.py
@@ -5,20 +5,32 @@ import login
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.parametrize('urls', ['https://stepik.org/lesson/236895/step/1'
-                                  'https://stepik.org/lesson/236896/step/1'
-                                  'https://stepik.org/lesson/236897/step/1'
-                                  'https://stepik.org/lesson/236898/step/1'
-                                  'https://stepik.org/lesson/236899/step/1'
-                                  'https://stepik.org/lesson/236903/step/1'
-                                  'https://stepik.org/lesson/236904/step/1'
+@pytest.mark.parametrize('urls', ['https://stepik.org/lesson/236895/step/1',
+                                  'https://stepik.org/lesson/236896/step/1',
+                                  'https://stepik.org/lesson/236897/step/1',
+                                  'https://stepik.org/lesson/236898/step/1',
+                                  'https://stepik.org/lesson/236899/step/1',
+                                  'https://stepik.org/lesson/236903/step/1',
+                                  'https://stepik.org/lesson/236904/step/1',
                                   'https://stepik.org/lesson/236905/step/1'])
 class TestStepik:
     def test_login_stepik(self, browser, urls):
-        answer = math.log(int(time.time()))
         link = urls
         browser.get(link)
         browser.find_element(By.XPATH, "//header[@id='main-header']//*[contains(@class, 'navbar__auth_login')]").click()
         browser.find_element(By.XPATH, "//form[@id='login_form']//*[contains(@name, 'login')]").send_keys(login.email)
         browser.find_element(By.XPATH, "//form[@id='login_form']//*[contains(@name, 'password')]").send_keys(login.password)
         browser.find_element(By.XPATH, "//form[@id='login_form']//*[contains(@type, 'submit')]").click()
+        browser.implicitly_wait(5)
+        browser.find_element(By.XPATH, "//*[contains(@class, 'string-quiz__textarea')]").send_keys(self.answer())
+        time.sleep(1)
+        browser.implicitly_wait(5)
+        browser.find_element(By.XPATH, "//*[contains(@class, 'submit-submission')]").click()
+        time.sleep(3)
+        browser.implicitly_wait(5)
+        correctly = browser.find_element(By.XPATH, "//*[@class='smart-hints__hint']")
+        assert correctly == 'Correct!'
+
+    def answer(self):
+        return str(math.log(int(time.time())))
+
